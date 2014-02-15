befae7467428ba169e0c7985d7413b40012b86f2
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2014-02-14 16:39:55 -0800

    dummy

--- TextWeather/settings.py
+++ TextWeather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'q_#mm&sz45rcm2biwk5%#@su#g^kij!zfim8vz89xx)un$k--0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
+DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
